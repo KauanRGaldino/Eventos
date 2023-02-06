8c0a97514399273cc026b7c392c519a1b5769feb
KAUAN RODRIGUES GALDINO <kauan.rgaldino@senacsp.edu.br>
2023-02-06 09:37:39 -0300

    first commit

---
+++ EX.js
@@ -0,0 +1,11 @@
+function aicalica(){
+    document.querySelector('div').innerText= "Passou o mouse" 
+}
+
+function aimessi(){
+    document.querySelector('div').innerText= "Clicou" 
+}
+
+function lobisomenpidao(){
+    document.querySelector('div').innerText= "Tirou o mouse" 
+}
