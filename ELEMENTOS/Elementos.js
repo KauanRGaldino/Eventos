e4513dcad07b45c96a146d537441b75706863fa4
KAUAN RODRIGUES GALDINO <kauan.rgaldino@senacsp.edu.br>
2023-02-07 11:26:55 -0300

    DOM

---
+++ ELEMENTOS/Elementos.js
@@ -0,0 +1,6 @@
+function kkk(){
+    const paragrafo = document.createElement('p')
+paragrafo.innerText = "Isso é um paragrafo"
+
+document.body.appendChild(paragrafo)
+}
---
+++ ELEMENTOS/classlist.js
@@ -0,0 +1,9 @@
+function aicalica(){
+document.querySelector("#minhaDiv").classList.add("classe1","classe2","classe3")
+
+}
+
+function laele(){
+    document.querySelector("#minhaDiv").classList.remove("classe1","classe2","classe3")
+    
+    }
