15ee5b89115d8b4a7bb89d728611b06fc37df2e2
KAUAN RODRIGUES GALDINO <kauan.rgaldino@senacsp.edu.br>
2023-02-08 09:06:21 -0300

    first commit

---
+++ DOM2.0/index.js
@@ -0,0 +1,19 @@
+function fundoAzul(){
+   let element = document.querySelector('#style-text')
+   element.style.backgroundColor = "blue"
+} 
+
+function fundoTransparente(){
+    let element = document.querySelector('#style-text')
+    element.style.backgroundColor = "transparent"
+} 
+
+function fonteVermelho(){
+    let element = document.querySelector('#style-text')
+    element.style.color = "red"
+} 
+
+function fonteNormal(){
+    let element = document.querySelector('#style-text')
+    element.style.color = "black"
+} 
